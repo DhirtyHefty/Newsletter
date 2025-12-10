67221ee89f9cead9bda8fd9ae08998c9e6b1a213
Gyasi Godwin <sarkcessfulhefy@gmail.com>
2025-12-10 18:24:09 +0000

    updating form inputs

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const  form = document.getElementById('newsletterForm');
-const submitButton = document.querySelector('submit-btn') ;
-const emailInputField = document.getElementById('email');
+document.addEventListener('DOMContentLoaded', function() {
 
-form.addEventListener('submit-btn', function(e) {
-    e.preventDefault();
-    console.log('you clicked me')
-})
+    //get elements
+    const form = document.getElementById('newsletterForm');
+    const submitButton = document.querySelector('.submit-btn');
+    const emailInputField = document.getElementById('email');
+
+    //add event listener to form
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        console.log('Form submitted');
+
+        //get email value
+        const email = emailInputField.value
+        console.log('email entered:', email)
+
+        //validate email
+        if(emailInputField.validity.valid){
+            console.log('Email is valid');
+        }else{
+            console.log('Email is invalid')
+            alert('Please enter a valid email')
+        }
+    });
+
+    
+});
